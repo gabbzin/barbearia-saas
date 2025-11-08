a4b396da0a2ab62b30c8651d51bd24ac1d284fac
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-11-08 11:09:18 -0300

    feat: add header and image components

---
+++ fsw-aparatus/app/_components/search-input.tsx
@@ -0,0 +1,20 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const SearchInput = () => {
+  return (
+    <div>
+      <Input
+        placeholder="Pesquise serviços ou barbearias"
+        type="text"
+        className="border-border rounded-full"
+      />
+      <Button variant={"default"} size={"icon"} className="rounded-full">
+        <SearchIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default SearchInput;
